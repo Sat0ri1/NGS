5c8630ee55a3001d1d9f4ca6a87edf2372285e98
Sat0ri1 <93346902+Sat0ri1@users.noreply.github.com>
2025-07-07 02:11:03 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e  # Exit if any command fails
+
+echo "Updating packages..."
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing Miniconda..."
+cd ~
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+echo " Initializing Conda..."
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+conda init
+
+
+echo "Installing Mamba into base environment..."
+conda install -y -n base -c conda-forge mamba
+echo " Initializing Mamba for shell integration..."
+mamba shell init --shell bash --root-prefix="$HOME/miniconda3"
+eval "$(mamba shell hook --shell bash)"
+
+echo "Installing unzip and zip"
+sudo apt install -y unzip zip
+
+echo " Installing Java (for Nextflow)..."
+echo "Installing sdk..."
+curl -s https://get.sdkman.io | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.9-tem
+
+
+
+
+echo " Installing Nextflow..."
+curl -s https://get.nextflow.io | bash
+chmod +x nextflow
+mkdir -p $HOME/.local/bin/
+mv nextflow $HOME/.local/bin/
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+export PATH="$HOME/.local/bin:$PATH"
+
+
+
+
+echo "Configuring Conda channels ..."
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda config --set channel_priority strict
+
+
+echo "Creating ngs environment..."
+
+
+
+mamba create -y -n ngs -c conda-forge -c bioconda   samtools   fastqc   bwa   trimmomatic   sra-tools   bcftools   gatk4   r-base  r-ggplot2 r-gplots r-gsalib multiqc
+
+# for downwloadding known sites from gooogle cloud
+sudo snap install google-cloud-sdk --classic
+echo "Done! Run ' source .bashrc and then mamba activate ngs and then  run ./biosetup.sh' to download and prepreocess test data."
+
+
+
